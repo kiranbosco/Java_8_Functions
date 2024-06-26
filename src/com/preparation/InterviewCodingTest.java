cce9d34f6e8b604eb1e001bbef1377888aa92349
kiran <kiranbosco@gmail.com>
2024-06-26 18:03:46 +0530

    Prime number logic added

--- src/com/preparation/InterviewCodingTest.java
+++ src/com/preparation/InterviewCodingTest.java
@@ -10,47 +10,47 @@ public class InterviewCodingTest {
 
     // hwo to dived add num and even num from the list
 
-    public static void devideOddAndEvennum(){
-        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,7);
+    public static void devideOddAndEvennum() {
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
         Map<Boolean, List<Integer>> integerListMap = integerList.stream()
-                .collect(partitioningBy(i->i%2==0));
+                .collect(partitioningBy(i -> i % 2 == 0));
         System.out.println(integerListMap);
     }
 
-    public static void avoidDuplicateValues(){
+    public static void avoidDuplicateValues() {
 
-        List<Integer> integerList = Arrays.asList(1,1,2,2,3,4);
+        List<Integer> integerList = Arrays.asList(1, 1, 2, 2, 3, 4);
         List<Integer> integersInfo = integerList.stream()
                 .distinct().collect(Collectors.toList());
         System.out.println(integersInfo);
 
     }
 
-    public static void sortingInList(){
-        List<String> stringList = Arrays.asList("Kiran","Manoja","Aarushi");
-        List<String> stringList1 =   stringList.stream()
+    public static void sortingInList() {
+        List<String> stringList = Arrays.asList("Kiran", "Manoja", "Aarushi");
+        List<String> stringList1 = stringList.stream()
                 .sorted()
                 .collect(Collectors.toList());
         System.out.println(stringList1);
     }
 
-    public static void reverseStringOrder(){
-        List<String> stringList = Arrays.asList("Az","By","Cx");
+    public static void reverseStringOrder() {
+        List<String> stringList = Arrays.asList("Az", "By", "Cx");
         List<String> reverse = stringList.stream()
                 .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
         System.out.println(reverse);
     }
 
-    public static void addPrefixAndSuffix_Joins(){
-        List<String> stringList = Arrays.asList("kumar","purini","kiran");
+    public static void addPrefixAndSuffix_Joins() {
+        List<String> stringList = Arrays.asList("kumar", "purini", "kiran");
         List<String> prfixexe = Collections.singletonList(stringList.stream()
-                .collect(Collectors.joining(",","[","]")));
+                .collect(Collectors.joining(",", "[", "]")));
         prfixexe.forEach(System.out::println);
     }
 
-    public static void fid_Max_Min_Numbers(){
-        List<Integer> maxMin = Arrays.asList(1,44,55,66,66);
+    public static void fid_Max_Min_Numbers() {
+        List<Integer> maxMin = Arrays.asList(1, 44, 55, 66, 66);
         Optional<Integer> maxNumber = maxMin.stream().max(Comparator.naturalOrder());
         Optional<Integer> minNumber = maxMin.stream().min(Comparator.naturalOrder());
 
@@ -59,29 +59,29 @@ public class InterviewCodingTest {
     }
     // how do you merge two arrays in singe array
 
-    public static void mergeTwoIntegerNumbersValues(){
-        int [] arrA = {1,2,3,};
-        int [] arrB = {4,5,6};
-        int[] arrC = IntStream.concat(Arrays.stream(arrA),Arrays.stream(arrB)).sorted().toArray();
+    public static void mergeTwoIntegerNumbersValues() {
+        int[] arrA = {1, 2, 3,};
+        int[] arrB = {4, 5, 6};
+        int[] arrC = IntStream.concat(Arrays.stream(arrA), Arrays.stream(arrB)).sorted().toArray();
         System.out.println(Arrays.toString(arrC));
     }
 
-    public static void unsortedTwovaluse_Merge_without_duplicates(){
+    public static void unsortedTwovaluse_Merge_without_duplicates() {
 
         int[] a = {4, 2, 5, 1};
-        int[] b =  {8, 1, 9, 5};
-        int [] c = IntStream.concat(Arrays.stream(a),Arrays.stream(b)).sorted().distinct().toArray();
+        int[] b = {8, 1, 9, 5};
+        int[] c = IntStream.concat(Arrays.stream(a), Arrays.stream(b)).sorted().distinct().toArray();
         System.out.println(Arrays.toString(c));
 
     }
 
     //How do you find common elements between two arrays?
 
-    public static void findcommonelementsbetween_two_arrays(){
-        List<String> stringList = Arrays.asList("kiran","purini");
-        List<String> stringList1 =Arrays.asList("kiran","Manoja");
+    public static void findcommonelementsbetween_two_arrays() {
+        List<String> stringList = Arrays.asList("kiran", "purini");
+        List<String> stringList1 = Arrays.asList("kiran", "Manoja");
 
-        List<String> list =   stringList.stream()
+        List<String> list = stringList.stream()
                 .filter(stringList1::contains)
                 .collect(Collectors.toList());
 
@@ -89,37 +89,53 @@ public class InterviewCodingTest {
 
     }
 
-    public static void reverseString(){
+    public static void reverseString() {
 
         String reverseInput = "PURINI";
         //CONVERT STRING TO CAR-ARRAY
-        char [] ch = reverseInput.toCharArray();
-        for(int i = ch.length-1; i>=0; i--){
+        char[] ch = reverseInput.toCharArray();
+        for (int i = ch.length - 1; i >= 0; i--) {
             System.out.print(ch[i]);
         }
 
     }
+
     //Anagram : if two string are equals are not check here?
     public static Boolean isStringAnagram(String str1, String str2) {
         // check if the condition is false return false
-        if(str1.length()!=str2.length()){
+        if (str1.length() != str2.length()) {
             return false;
         }
         // convert charArrays
         char ch[] = str1.toCharArray();
-        char ch1 []= str2.toCharArray();
+        char ch1[] = str2.toCharArray();
 
         Arrays.sort(ch);
         Arrays.sort(ch1);
-        return Arrays.equals(ch,ch1);
+        return Arrays.equals(ch, ch1);
     }
 
-    public static int sumOf2_num(List<Integer> sumNumb){
-        return sumNumb.stream().reduce(1,(a,b)->a*b);
-
-
+    public static int sumOf2_num(List<Integer> sumNumb) {
+        return sumNumb.stream().reduce(1, (a, b) -> a * b);
     }
 
+    //find ovel in this method]
+    public static boolean checkVovel_in_string(String str) {
+        return str.matches(".*[aeiou]*.");
+    }
+
+    // find the prime number in java
+    public static boolean givenNumber_is_primeOrNot(Integer num) {
+// if prime number is divisible by 1 and itself
+
+        for(int i=2; i<num; i++){
+            if(num%1==0){
+                return false ; // if given number became O this is not a prime number
+            }
+        }
+        return true;
+
+    }
 
     public static void main(String[] args) {
         //   List<Integer> sumNumb =Arrays.asList(1,2,3,4);
@@ -136,6 +152,7 @@ public class InterviewCodingTest {
         // System.out.println(isStringAnagram("eat","tea"));
         //  unsortedTwovaluse_Merge_without_duplicates
         //     System.out.println(sumOf2_num(sumNumb));
-
+        //    System.out.println(checkVovel_in_string("mano"));
+        System.out.println(givenNumber_is_primeOrNot(3));
     }
 }
